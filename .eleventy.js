566c8979d52a8907cee2a9a653636dd10df4857a
Ulrich Alt <ualt@west-gmbh.de>
2022-06-22 08:55:54 +0200

    Adding RSS feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation"); 
-const
-Image = require("@11ty/eleventy-img");
+const Image = require("@11ty/eleventy-img");
+const { DateTime } = require("luxon");
 
 async function imageShortCode(src,alt) { let metadata = await Image(src, {
     widths: [300,600,1000], outputDir: "./_site/img", formats: ["avif", "jpeg"] });
@@ -13,18 +13,15 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
     eleventyConfig.addPlugin(require("eleventy-plugin-postcss"));
     eleventyConfig.addPlugin(require("@sherby/eleventy-plugin-files-minifier"));
+    eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-rss"));
     eleventyConfig.addNunjucksAsyncShortcode("image", imageShortCode);
     eleventyConfig.addPassthroughCopy("src/icons");
-
-
-  const { DateTime } = require("luxon");
-
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
     eleventyConfig.addFilter('htmlDateString', (dateObj) => { return
-        DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yy-MM-dd'); });
+    DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yy-MM-dd'); });
 
     eleventyConfig.addFilter("readableDate", dateObj => { return
         DateTime.fromJSDate(dateObj, {}).setLocale('de-de').toLocaleString( {
-            month: '2-digit', year: '2-digit', day: '2-digit' }); });
+        month: '2-digit', year: '2-digit', day: '2-digit' }); });
 
-  return { markdownTemplateEngine: "njk", dir: { input: 'src', output: '_site' } }; };
+    return { markdownTemplateEngine: "njk", dir: { input: 'src', output: '_site' } };
+};
